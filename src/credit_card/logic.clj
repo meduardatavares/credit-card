db9bed8fcdfb4eb5ebec2bd81a8d1313f9fadf16
Maria Eduarda Moreira Tavares <eduarda.tavares@nubank.com.br>
2021-12-03 17:53:02 -0300

    validação com schemas

--- src/credit_card/logic.clj
+++ src/credit_card/logic.clj
@@ -1,56 +1,89 @@
 (ns credit_card.logic
   (:require [credit_card.db :as l.db]
-            [java-time :as time])
-  (:use [clojure pprint]))
+            [java-time :as time]
+            [schema.core :as s])
+  (:use [clojure pprint])
+  (:import java.time.LocalDate
+           (java.time Month Year)))
 
-(defn valor-total-das-compras
-  [compras]
-  (->> compras
-       (map :valor)
-       (reduce +)))
+(s/set-fn-validation! true)
 
-(defn valor-total-das-compras-por-categoria
-  [[categoria compras]]
+(def Compra
+  {:id              s/Keyword
+   :data            LocalDate
+   :valor           s/Num
+   :estabelecimento s/Str
+   :categoria       s/Str
+
+   })
+
+(def Compras
+  [Compra])
+
+(def Cartao
+  {:numero             s/Num
+   :cvv                s/Num
+   :validade           LocalDate
+   :limite             s/Num
+   :compras-realizadas Compras
+
+   })
+(def Cliente
+  {:nome   s/Str
+   :cpf    s/Num
+   :email  s/Str
+   :cartao Cartao})
+
+
+
+(s/defn valor-total-das-compras
+        [compras :- Compras]
+        (->> compras
+             (map :valor)
+             (reduce +)))
+
+(s/defn valor-total-das-compras-por-categoria
+  [[categoria  compras]]
   {categoria
    (valor-total-das-compras compras)})
 
-(defn compras-realizadas-cliente
-  [cliente]
-  (:compras-realizadas (:cartao cliente)))
+(s/defn compras-realizadas-cliente
+        [cliente :- Cliente]
+        (:compras-realizadas (:cartao cliente)))
 
-(defn gastos-por-categoria
-  [cliente]
-  (->> (compras-realizadas-cliente cliente)
-       (group-by :categoria)
-       (map valor-total-das-compras-por-categoria)
-       ))
+(s/defn gastos-por-categoria
+   [cliente :- Cliente]
+   (->> (compras-realizadas-cliente cliente)
+             (group-by :categoria)
+             (map valor-total-das-compras-por-categoria)
+        ))
 
-(defn fatura-do-mes
-  [compras mes ano]
+(s/defn fatura-do-mes
+  [compras :- Compras mes :- Month ano :- Year]
   (->> compras
-       (filter #(and (= (time/month (:data %)) mes)  (= (time/year (:data %)) ano)))
+       (filter #(and (= (time/month (:data %)) mes) (= (time/year (:data %)) ano)))
        (map :valor)
        (reduce +)
        (println (time/.name mes) (time/.getValue ano) "- valor ")))
 
-(defn fatura-dos-meses-por-ano-atual
-  [cliente]
+(s/defn fatura-dos-meses-por-ano-atual
+  [cliente :- Cliente]
   (for [x (range 1 13)]
     (fatura-do-mes (compras-realizadas-cliente cliente) (time/month x) (time/year (time/local-date)))))
 
-(defn compras-por-valor
-  [cliente valor]
+(s/defn compras-por-valor
+  [cliente :- Cliente valor :- s/Num]
   (->> (compras-realizadas-cliente cliente)
-       (filter #(and (>= (:valor %) valor) (> valor 0 ))
-       )))
+       (filter #(and (>= (:valor %) valor) (> valor 0))
+               )))
 
-(defn compras-por-estabelecimento
-  [cliente estabelecimento]
+(s/defn compras-por-estabelecimento
+  [cliente :- Cliente estabelecimento :- s/Str]
   (->> (compras-realizadas-cliente cliente)
        (filter #(= (:estabelecimento %) estabelecimento))
        ))
 
-(defn adiciona-compra [cliente nova-compra]
+(s/defn adiciona-compra [cliente :- Cliente nova-compra :- Compra]
   (conj (compras-realizadas-cliente cliente) nova-compra))
 
 
--- test/credit_card/logic_test.clj
+++ test/credit_card/logic_test.clj
@@ -3,167 +3,180 @@
             [credit_card.logic :refer :all]
             [credit_card.db :refer :all]
             [java-time :as time]))
-(def mock1  {:nome "mateus"
-             :cpf 12345678
-             :email "joao@gmail.com"
-             :cartao {:numero 5274639975504476
-                      :cvv 564
-                      :validade (time/local-date 2029 05 18 )
-                      :limite 1000
-                      :compras-realizadas [{:id :compra1
-                                            :data "01/01/2021"
-                                            :valor 50.0
-                                            :estabelecimento "ABC"
-                                            :categoria "Alimentação"}
-                                           {:id :compra2
-                                            :data (time/local-date 2021 02 14 )
-                                            :valor 200.0
-                                            :estabelecimento "Padaria"
-                                            :categoria "Alimentação"}
-                                           {:id :compra3
-                                            :data (time/local-date 2021 01 22 )
-                                            :valor 100.0
-                                            :estabelecimento "supermercdo"
-                                            :categoria "Alimentação"}
-                                           {:id :compra4
-                                            :data "10/09/2021"
-                                            :valor 200.0
-                                            :estabelecimento "Curso wizard"
-                                            :categoria "Alimentação"}
-                                           {:id :compra5
-                                            :data (time/local-date 2021 02 10 )
-                                            :valor 200.0
-                                            :estabelecimento "Colegio Meta"
-                                            :categoria "Alimentação"}
-                                           {:id :compra6
-                                            :data (time/local-date 2021 02 10 )
-                                            :valor 600.0
-                                            :estabelecimento "Hospital Santa Casa"
-                                            :categoria "Alimentação"}
-                                           {:id :compra7
-                                            :data (time/local-date 2021 01 7 )
-                                            :valor 200.0
-                                            :estabelecimento "Odonto Clear"
-                                            :categoria "Alimentação"}
-                                           {:id :compra8
-                                            :data "10/10/2021"
-                                            :valor 250.0
-                                            :estabelecimento "Curso wizard"
-                                            :categoria "Alimentação"}] }})
-(def mock2  {:nome "julia"
-             :cpf 12345678
-             :email "joao@gmail.com"
-             :cartao {:numero 5274639975504476
-                      :cvv 564
-                      :validade (time/local-date 2029 05 18 )
-                      :limite 1000
-                      :compras-realizadas [] }})
-(def mock3  {:nome "vanessa"
-             :cpf 12345678
-             :email "joao@gmail.com"
-             :cartao {:numero 5274639975504476
-                      :cvv 564
-                      :validade (time/local-date 2029 05 18 )
-                      :limite 1000
-                      :compras-realizadas [{:id :compra1,
-                                            :data "01/01/2021",
-                                            :valor 50.0,
-                                            :estabelecimento "ABC",
-                                            :categoria "Alimentação"}] }})
+(def mock1 {:nome   "mateus"
+            :cpf    123
+            :email  "joao@gmail.com"
+            :cartao {:numero             5274639975504476
+                     :cvv                564
+                     :validade           (time/local-date 2029 05 18)
+                     :limite             1000
+                     :compras-realizadas [{:id              :compra1
+                                           :data            (time/local-date 2021 01 01)
+                                           :valor           50.0
+                                           :estabelecimento "ABC"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra2
+                                           :data            (time/local-date 2021 02 14)
+                                           :valor           200.0
+                                           :estabelecimento "Padaria"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra3
+                                           :data            (time/local-date 2021 01 22)
+                                           :valor           100.0
+                                           :estabelecimento "supermercdo"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra4
+                                           :data            (time/local-date 2021 9 10)
+                                           :valor           200.0
+                                           :estabelecimento "Curso wizard"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra5
+                                           :data            (time/local-date 2021 02 10)
+                                           :valor           200.0
+                                           :estabelecimento "Colegio Meta"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra6
+                                           :data            (time/local-date 2021 02 10)
+                                           :valor           600.0
+                                           :estabelecimento "Hospital Santa Casa"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra7
+                                           :data            (time/local-date 2021 01 7)
+                                           :valor           200.0
+                                           :estabelecimento "Odonto Clear"
+                                           :categoria       "Alimentação"}
+                                          {:id              :compra8
+                                           :data            (time/local-date 2021 10 10)
+                                           :valor           250.0
+                                           :estabelecimento "Curso wizard"
+                                           :categoria       "Alimentação"}]}})
+(def mock2 {:nome   "julia"
+            :cpf    12345678
+            :email  "joao@gmail.com"
+            :cartao {:numero             5274639975504476
+                     :cvv                564
+                     :validade           (time/local-date 2029 05 18)
+                     :limite             1000
+                     :compras-realizadas []}})
+(def mock3 {:nome   "vanessa"
+            :cpf    12345678
+            :email  "joao@gmail.com"
+            :cartao {:numero             5274639975504476
+                     :cvv                564
+                     :validade           (time/local-date 2029 05 18)
+                     :limite             1000
+                     :compras-realizadas [{:id              :compra1,
+                                           :data            (time/local-date 2029 05 18)
+                                           :valor           50.0,
+                                           :estabelecimento "ABC",
+                                           :categoria       "Alimentação"}]}})
 
-(deftest  compras-por-estabelecimento-test
-  ( testing "compras por estabelecimento"
-    (is (= [] (compras-por-estabelecimento cliente1 "Colegio")) )
-    (is (=  [{:id :compra1, :data (time/local-date 2021 01 01 ), :valor 50.0, :estabelecimento "ABC", :categoria "Alimentação"}]  (compras-por-estabelecimento cliente1 "ABC")) )
-    (is (=  [ {:id :compra4
-               :data (time/local-date 2021 01 22)
-               :valor 200.0
-               :estabelecimento "Curso wizard"
-               :categoria "Educação"}
-             {:id :compra8
-              :data (time/local-date 2021 10 10 )
-              :valor 250.0
-              :estabelecimento "Curso wizard"
-              :categoria "Educação"}
-            ]  (compras-por-estabelecimento cliente1 "Curso wizard")) )
+(deftest compras-por-estabelecimento-test
+  (testing "compras por estabelecimento"
+    (is (= [] (compras-por-estabelecimento mock3 "Colegio")))
+    (is (= [{:id :compra1, :data (time/local-date 2021 01 01), :valor 50.0, :estabelecimento "ABC", :categoria "Alimentação"}] (compras-por-estabelecimento mock1 "ABC")))
+    (is (= [{:id              :compra4
+             :data            (time/local-date 2021 01 22)
+             :valor           200.0
+             :estabelecimento "Curso wizard"
+             :categoria       "Educação"}
+            {:id              :compra8
+             :data            (time/local-date 2021 10 10)
+             :valor           250.0
+             :estabelecimento "Curso wizard"
+             :categoria       "Educação"}
+            ] (compras-por-estabelecimento cliente1 "Curso wizard")))
     ))
+
 (deftest gastos-por-categoria-test
   (testing "Tem gastos na categoria"
     (is (= [{"Alimentação" 350.0} {"Educação" 650.0} {"Saude" 800.0}] (gastos-por-categoria cliente1)))
-    (is (= [{"Alimentação" 1800.0} ] (gastos-por-categoria mock1)))
+    (is (= [{"Alimentação" 1800.0}] (gastos-por-categoria mock1)))
     (is (= [] (gastos-por-categoria mock2)))
     ))
 (deftest compras-realizadas-cliente-test
   (testing "Compras realizadas pelo cliente"
-    (is (= [{:id :compra1,
-             :data "01/01/2021",
-             :valor 50.0,
+    (is (= [{:id              :compra1,
+             :data            (time/local-date 2029 05 18)
+             :valor           50.0,
              :estabelecimento "ABC",
-             :categoria "Alimentação"}] (compras-realizadas-cliente mock3)))
+             :categoria       "Alimentação"}] (compras-realizadas-cliente mock3)))
     (is (= [] (compras-realizadas-cliente mock2)))
-    (is (= [{:id :compra1
-             :data "01/01/2021"
-             :valor 50.0
+    (is (= [{:id              :compra1
+             :data            (time/local-date 2021 01 01)
+             :valor           50.0
              :estabelecimento "ABC"
-             :categoria "Alimentação"}
-            {:id :compra2
-             :data (time/local-date 2021 02 14 )
-             :valor 200.0
+             :categoria       "Alimentação"}
+            {:id              :compra2
+             :data            (time/local-date 2021 02 14)
+             :valor           200.0
              :estabelecimento "Padaria"
-             :categoria "Alimentação"}
-            {:id :compra3
-             :data (time/local-date 2021 01 22 )
-             :valor 100.0
+             :categoria       "Alimentação"}
+            {:id              :compra3
+             :data            (time/local-date 2021 01 22)
+             :valor           100.0
              :estabelecimento "supermercdo"
-             :categoria "Alimentação"}
-            {:id :compra4
-             :data "10/09/2021"
-             :valor 200.0
+             :categoria       "Alimentação"}
+            {:id              :compra4
+             :data            (time/local-date 2021 9 10)
+             :valor           200.0
              :estabelecimento "Curso wizard"
-             :categoria "Alimentação"}
-            {:id :compra5
-             :data (time/local-date 2021 02 10 )
-             :valor 200.0
+             :categoria       "Alimentação"}
+            {:id              :compra5
+             :data            (time/local-date 2021 02 10)
+             :valor           200.0
              :estabelecimento "Colegio Meta"
-             :categoria "Alimentação"}
-            {:id :compra6
-             :data (time/local-date 2021 02 10 )
-             :valor 600.0
+             :categoria       "Alimentação"}
+            {:id              :compra6
+             :data            (time/local-date 2021 02 10)
+             :valor           600.0
              :estabelecimento "Hospital Santa Casa"
-             :categoria "Alimentação"}
-            {:id :compra7
-             :data (time/local-date 2021 01 7 )
-             :valor 200.0
+             :categoria       "Alimentação"}
+            {:id              :compra7
+             :data            (time/local-date 2021 01 7)
+             :valor           200.0
              :estabelecimento "Odonto Clear"
-             :categoria "Alimentação"}
-            {:id :compra8
-             :data "10/10/2021"
-             :valor 250.0
+             :categoria       "Alimentação"}
+            {:id              :compra8
+             :data            (time/local-date 2021 10 10)
+             :valor           250.0
              :estabelecimento "Curso wizard"
-             :categoria "Alimentação"}] (compras-realizadas-cliente mock1)))
+             :categoria       "Alimentação"}] (compras-realizadas-cliente mock1)))
     ))
 (deftest compras-por-valor-test
   (testing "filtrar compras pelo valor"
-    (is (= [{:id :compra6
-             :data (time/local-date 2021 02 10 )
-             :valor 600.0
+    (is (= [{:id              :compra6
+             :data            (time/local-date 2021 02 10)
+             :valor           600.0
              :estabelecimento "Hospital Santa Casa"
-             :categoria "Alimentação"}
-            {:id :compra8
-             :data "10/10/2021"
-             :valor 250.0
+             :categoria       "Alimentação"}
+
+            {:id              :compra8
+             :data            (time/local-date 2021 10 10)
+             :valor           250.0
              :estabelecimento "Curso wizard"
-             :categoria "Alimentação"}
-            ] (compras-por-valor mock1 250 )))
-    (is (= [] (compras-por-valor mock1 1000 )))
-    (is (= [] (compras-por-valor mock1 -600 )))
+             :categoria       "Alimentação"}
+            ] (compras-por-valor mock1 250)))
+    (is (= [] (compras-por-valor mock1 1000)))
+    (is (= [] (compras-por-valor mock1 -600)))
     ))
 
 (deftest adiciona-compra-test
   (testing "Retorna lista com novas compras"
-    (is (= count (adiciona-compra mock2 {:id :compra1
-                                      :data "10/10/2021"
-                                      :valor 250.0
-                                      :estabelecimento "Curso wizard"
-                                      :categoria "Alimentação"})  ))))
+    (is (= [{:id              :compra1
+             :data            (time/local-date 2021 10 10)
+             :valor           250.0
+             :estabelecimento "Curso wizard"
+             :categoria       "Alimentação"}] (adiciona-compra mock2 {:id              :compra1
+                                                                      :data            (time/local-date 2021 10 10)
+                                                                      :valor           250.0
+                                                                      :estabelecimento "Curso wizard"
+                                                                      :categoria       "Alimentação"}))))
+  (testing "retorna a quantidade mais um de compras"
+    (is (= (+ (count (:compras-realizadas (:cartao mock1))) 1)
+           (count (adiciona-compra mock1 {:id              :compra1
+                                          :data            (time/local-date 2021 10 10)
+                                          :valor           250.0
+                                          :estabelecimento "Curso wizard"
+                                          :categoria       "Alimentação"}))))))
 
